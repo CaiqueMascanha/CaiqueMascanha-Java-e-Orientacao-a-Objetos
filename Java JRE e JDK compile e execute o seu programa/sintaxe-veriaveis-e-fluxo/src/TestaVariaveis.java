ca1befa902524f63e9a8f879f3cc647c6ed4bb5a
Caique Mascanha <caiquesilva820@gmail.com>
2023-02-01 19:01:07 -0300

    Primeiro commit

---
+++ Java JRE e JDK compile e execute o seu programa/sintaxe-veriaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,13 @@
+
+public class TestaVariaveis {
+
+	/*sempre temos que começar o código com public static void main(String[] args)*/
+	public static void main(String[] args) {
+		System.out.println("Ola, novo teste");
+		
+		int idade;
+		idade = 21;
+		
+		System.out.println(idade);
+	}
+}
